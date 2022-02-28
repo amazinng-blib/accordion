d7c85a473ac566142f47185f713ce50794112442
Nwankwo Ernest <nwankwoernest2020@gmail.com>
2022-02-28 15:16:29 -0800

    Accordion Project With React.Js

---
+++ src/accordion/Question.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  // const getInfo = () => {
+  //   setShowInfo((prev) => !prev);
+  // };
+  return (
+    <article className="question">
+      <header className="header">
+        <h4>{title}</h4>
+
+        <button onClick={() => setShowInfo(() => !showInfo)} className="btn">
+          {" "}
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}{" "}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+};
+
+export default Question;
